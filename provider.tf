35da02bb3c62abd4abd540f887b1536bbd385ea7
Prav-Akula <pravs.akula@outlook.com>
2026-04-09 13:04:01 -0400

    Jenkins

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "daws86s-roboshop-infra-dev "
+    bucket = "daws86s-roboshop-infra-dev"
     key    = "roboshop-dev-jenkins"
     region = "us-east-1"
     use_lockfile = true
